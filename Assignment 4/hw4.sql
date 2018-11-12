964bb9e14a738c8de575aff1668dd78eea0a65b9
Chris Panican <chrispanican@gmail.com>
2018-11-12 12:15:57 -0500

    Assignment 4 part 2

---
+++ Assignment 4/hw4.sql
@@ -0,0 +1,56 @@
+-- Queries to answer #2
+
+CREATE TEMP TABLE assign_unique_dates AS (
+    WITH remove_duplicate_dates AS (
+        SELECT DISTINCT "date"
+        FROM prices
+        ORDER BY "date"
+    )
+    SELECT
+        "date",
+        ROW_NUMBER() OVER (
+            PARTITION BY EXTRACT("year" FROM "date")
+            ORDER BY "date" DESC
+        )
+    FROM remove_duplicate_dates
+    ORDER BY "date"
+);
+
+CREATE TEMP TABLE year_end_prices AS (
+    WITH year_end_dates AS (
+        SELECT "date"
+        FROM assign_unique_dates
+        WHERE row_number = 1
+        ORDER BY "date"
+    )
+    SELECT 
+        symbol,
+        EXTRACT("year" FROM "date") AS "year",
+        "close" AS end_price
+    FROM prices
+    WHERE "date" IN (
+        SELECT * 
+        FROM year_end_dates
+    )
+    ORDER by symbol, date
+);
+
+CREATE TEMP TABLE best_year_returns AS (
+    SELECT 
+        "symbol",
+        "year",
+        LAG(end_price) OVER (
+            PARTITION BY "symbol"
+            ORDER BY "year"
+        ) AS "start_price",
+        end_price,
+        (end_price/LAG(end_price) OVER (
+            PARTITION BY "symbol"
+            ORDER BY "year")) - 1
+        AS annual_returns
+    FROM year_end_prices
+);
+
+SELECT * FROM best_year_returns
+WHERE annual_returns IS NOT NULL
+ORDER BY annual_returns DESC;
