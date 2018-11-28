c11f80323e411d045ac34a7433826c3c509b59b6
Chris Panican <chrispanican@gmail.com>
2018-11-28 15:09:45 -0500

    Cleanup

--- Assignment 5/hw5.sql
+++ Assignment 5/hw5.sql
@@ -1,38 +1,19 @@
--- Part 1
--- Do the high-performers have any of the following in common?
+\echo 'Part 1'
+\echo 'Do the high-performers have any of the following in common?\n'
 
--- -- Helper functions
--- -- Get last business (trading) day of the year for each year (the last day we have data for in the data set).
--- CREATE TEMP TABLE partitioned_dates AS
--- SELECT 
---     tdate, 
---     ROW_NUMBER() OVER (PARTITION BY DATE_TRUNC('year', tdate) ORDER BY tdate DESC) AS num
---     -- could use LAST_VALUE() or FIRST_VALUE() here ...
--- FROM prices
--- WHERE symbol LIKE 'A%'  -- Speedup hack - don't need to check every single symbol. (30ms vs 371ms)
--- ;
 
--- -- Set up list of last TRADING DAY of year (not last DAY - last TRADING DAY - that's IN the DATA !).
--- CREATE TEMP TABLE year_ends AS
--- SELECT tdate AS year_ends
--- FROM partitioned_dates
--- WHERE num = 1
--- ORDER BY tdate DESC
--- ;
-
--- Setup Temp table for awesome_fundamentals
+\echo 'Setup Temp table for awesome_fundamentals\n'
 CREATE TEMP TABLE awesome_fundamentals AS
     SELECT t1.* FROM fundamentals t1
     INNER JOIN awesome_performers t2 
         ON t1.symbol = t2.symbol
     ORDER BY t1.symbol;
 
--- select * from awesome_fundamentals limit 10;
+select * from awesome_fundamentals limit 10;
 
 
-
--- High net worth (total assets - total expenses)
---     Or: (even better) high net worth growth year-over-year
+\echo '\nHigh net worth (total assets - total expenses)'
+\echo '     Or: (even better) high net worth growth year-over-year\n'
 CREATE TEMP TABLE net_worth_yearly AS
     SELECT
         symbol,
@@ -42,11 +23,10 @@ CREATE TEMP TABLE net_worth_yearly AS
         ))::NUMERIC(10,2) - 1 AS net_growth
     FROM awesome_fundamentals;
 -- Show top 10 companies with positive net growth.
--- SELECT * FROM net_worth_yearly WHERE net_growth > 0 ORDER BY net_growth DESC LIMIT 10;
+SELECT * FROM net_worth_yearly WHERE net_growth > 0 ORDER BY net_growth DESC LIMIT 10;
 
 
-
--- High net income growth year-over-year?
+\echo '\nHigh net income growth year-over-year?\n'
 CREATE TEMP TABLE net_income_yearly AS
     SELECT
         symbol,
@@ -56,11 +36,10 @@ CREATE TEMP TABLE net_income_yearly AS
         ))::NUMERIC(10,2) - 1 AS net_inc_growth
     FROM awesome_fundamentals;
 -- Show top 10 companies with positive net income growth
--- SELECT * FROM net_income_yearly WHERE net_inc_growth > 0 ORDER BY net_inc_growth DESC LIMIT 10;
+SELECT * FROM net_income_yearly WHERE net_inc_growth > 0 ORDER BY net_inc_growth DESC LIMIT 10;
 
 
-
--- High revenue growth year-over-year?
+\echo '\nHigh revenue growth year-over-year?\n'
 CREATE TEMP TABLE revenue_growth_yearly AS
     SELECT
         symbol,
@@ -70,12 +49,11 @@ CREATE TEMP TABLE revenue_growth_yearly AS
         ))::NUMERIC(10,2) - 1 AS revenue_growth
     FROM awesome_fundamentals;
 -- Show top 10 companies with positive revenue growth
--- SELECT * FROM revenue_growth_yearly WHERE revenue_growth > 0 ORDER BY revenue_growth DESC LIMIT 10;
+SELECT * FROM revenue_growth_yearly WHERE revenue_growth > 0 ORDER BY revenue_growth DESC LIMIT 10;
 
 
-
--- High earnings-per-share?
---     High earnings-per-share (eps) growth?
+\echo '\nHigh earnings-per-share?'
+\echo '    High earnings-per-share (eps) growth?\n'
 CREATE TEMP TABLE eps_growth_yearly AS
     SELECT
         symbol,
@@ -85,12 +63,11 @@ CREATE TEMP TABLE eps_growth_yearly AS
         ))::NUMERIC(10,2) - 1 AS eps_growth
     FROM awesome_fundamentals;
 -- Show top 10 companies with positive eps growth
--- SELECT * FROM eps_growth_yearly WHERE eps_growth > 0 ORDER BY eps_growth DESC LIMIT 10;
+SELECT * FROM eps_growth_yearly WHERE eps_growth > 0 ORDER BY eps_growth DESC LIMIT 10;
 
 
-
--- Low price-to-earnings ratio?
---     (this is stock price relative to eps - i.e.: pe ratio = share price / eps)
+\echo '\nLow price-to-earnings ratio?'
+\echo '    (this is stock price relative to eps - i.e.: pe ratio = share price / eps)\n'
 CREATE TEMP TABLE price_to_earning AS
     SELECT
         t1.symbol,
@@ -101,11 +78,10 @@ CREATE TEMP TABLE price_to_earning AS
         ON t1.year_ending = t2.date AND t1.symbol = t2.symbol
     ORDER BY t1.symbol;
 -- Show top 10 companies and year with the highest price to earning ratio
--- SELECT * FROM price_to_earning WHERE price_to_earning_ratio > 0 ORDER BY price_to_earning_ratio DESC LIMIT 10;
+SELECT * FROM price_to_earning WHERE price_to_earning_ratio > 0 ORDER BY price_to_earning_ratio DESC LIMIT 10;
 
 
-
--- Amount of liquid cash in the bank vs. total liabilities?
+\echo '\nAmount of liquid cash in the bank vs. total liabilities?\n'
 CREATE TEMP TABLE liquid_vs_liabilities AS
     SELECT
         symbol,
@@ -113,16 +89,16 @@ CREATE TEMP TABLE liquid_vs_liabilities AS
         (total_liabilities/cash_and_cash_equivalents::NUMERIC) AS liquid_liabilities_ratio
     FROM awesome_fundamentals;
 -- Show top 10 companies with positive liquid asset and liabilities ratio
--- SELECT * from liquid_vs_liabilities ORDER BY liquid_liabilities_ratio DESC LIMIT 10;
+SELECT * from liquid_vs_liabilities ORDER BY liquid_liabilities_ratio DESC LIMIT 10;
 
 
 
--- Part 2
+\echo '\nPart 2'
 
--- These factors had a huge contribution to high performance of all companies:
---     1. net_income_yearly
---     2. revenue_growth_yearly
---     3. eps_growth_yearly
+\echo 'These factors had a huge contribution to high performance of all companies:'
+\echo '    1. net_income_yearly'
+\echo '    2. revenue_growth_yearly'
+\echo '    3. eps_growth_yearly\n'
 
 CREATE TEMP TABLE potential_candidates AS
     SELECT
@@ -183,6 +159,3 @@ WHERE
 -- 9. DHI  | D. R. Horton
 -- 10. DIS | The Walt Disney Company
 
-
-
--- Extra credit
