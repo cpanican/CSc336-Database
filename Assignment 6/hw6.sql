1ea73ca13d87872fef5f84098701bc0243a40125
Chris Panican <chrispanican@gmail.com>
2018-12-03 12:45:19 -0500

    Add base file ;

---
+++ Assignment 6/hw6.sql
@@ -0,0 +1,22 @@
+-- Companies Chosen from HW 5
+
+-- Information Technology
+-- 1. CSCO | Cisco Systems
+-- 2. INTU | Intuit Inc.
+
+-- Health Care
+-- 3. CAH  | Cardinal Health Inc.
+-- 4. HOLX | Hologic
+
+-- Industrials
+-- 5. AYI  | Acuity Brands Inc
+-- 6. FDX  | FedEx Corporation
+
+-- Consumer Staples
+-- 7. SJM  | JM Smucker
+-- 8. SYY  | Sysco Corp.
+
+-- Consumer Discretionary
+-- 9. DHI  | D. R. Horton
+-- 10. DIS | The Walt Disney Company
+
