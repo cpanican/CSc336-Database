60b8cf324ea167b2a443cac107c1e38f2e1dc3a8
Chris Panican <chrispanican@gmail.com>
2018-12-20 16:29:39 -0500

    Finish data setup

--- Assignment 7/data_setup.sql
+++ Assignment 7/data_setup.sql
@@ -3,16 +3,15 @@ BEGIN;
 DROP TABLE IF EXISTS census CASCADE;
 
 CREATE TABLE census (
-    zip_code integer PRIMARY KEY NOT NULL,
+    zip_code numeric(5) PRIMARY KEY NOT NULL,
     total_population integer NOT NULL,
     median_age numeric(10,1) NOT NULL,
     total_males integer NOT NULL,
     total_females integer NOT NULL,
     total_households integer NOT NULL,
-    avg_household_size numeric(10,1) NOT NULL
+    avg_household_size numeric(10,2) NOT NULL
 );
 
-\COPY locations FROM './census.csv' WITH (FORMAT csv);
+\COPY census FROM './census.csv' WITH (FORMAT csv);
 
 COMMIT;
-ANALYZE census;
